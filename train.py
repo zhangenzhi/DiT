0e74d9b63022417c676a22bd669eeb36f0a35040
Enzhi Zhang <zhangenzhi657@gmail.com>
2026-01-09 10:46:45 +0900

    test sampling.

--- train.py
+++ train.py
@@ -36,19 +36,36 @@ from download import resume_from_checkpoint
 #                             Training Helper Functions                         #
 #################################################################################
 
+# @torch.no_grad()
+# def update_ema(ema_model, model, decay=0.9999):
+#     """
+#     Step the EMA model towards the current model.
+#     """
+#     ema_params = OrderedDict(ema_model.named_parameters())
+#     model_params = OrderedDict(model.named_parameters())
+
+#     for name, param in model_params.items():
+#         # TODO: Consider applying only to params that require_grad to avoid small numerical changes of pos_embed
+#         ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay)
+
 @torch.no_grad()
 def update_ema(ema_model, model, decay=0.9999):
     """
     Step the EMA model towards the current model.
+    Includes a fix for torch.compile which adds '_orig_mod.' prefix.
     """
     ema_params = OrderedDict(ema_model.named_parameters())
     model_params = OrderedDict(model.named_parameters())
 
     for name, param in model_params.items():
-        # TODO: Consider applying only to params that require_grad to avoid small numerical changes of pos_embed
-        ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay)
-
-
+        # [FIX] 移除 torch.compile 产生的 _orig_mod. 前缀
+        if name.startswith("_orig_mod."):
+            name = name.replace("_orig_mod.", "")
+            
+        # 确保 key 存在于 EMA 模型中才更新
+        if name in ema_params:
+            ema_params[name].mul_(decay).add_(param.data, alpha=1 - decay)
+            
 def requires_grad(model, flag=True):
     """
     Set requires_grad flag for all parameters in a model.
