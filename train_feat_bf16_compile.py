cdc89d7b19518d0c3411f4b81a2a79b73024c6e5
Enzhi Zhang <zhangenzhi657@gmail.com>
2026-01-13 10:08:34 +0900

    test torch cimpile

--- train_feat_bf16_compile.py
+++ train_feat_bf16_compile.py
@@ -273,7 +273,7 @@ if __name__ == "__main__":
     parser.add_argument("--global-batch-size", type=int, default=256)
     parser.add_argument("--global-seed", type=int, default=0)
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema") 
-    parser.add_argument("--num-workers", type=int, default=4)
+    parser.add_argument("--num-workers", type=int, default=32)
     parser.add_argument("--log-every", type=int, default=100)
     parser.add_argument("--ckpt-every", type=int, default=50_000)
     args = parser.parse_args()
