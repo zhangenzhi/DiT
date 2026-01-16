dbcd9869d4a979374628c77bcf0271aa1b70919d
zhangenzhi <zhangenzhi657@gmail.com>
2026-01-16 18:51:44 +0900

    snr is valid.

--- train_feat_bf16_cp_bz_snr.py
+++ train_feat_bf16_cp_bz_snr.py
@@ -333,7 +333,7 @@ if __name__ == "__main__":
     parser.add_argument("--image-size", type=int, choices=[256, 512], default=256)
     parser.add_argument("--num-classes", type=int, default=1000)
     parser.add_argument("--epochs", type=int, default=400)
-    parser.add_argument("--global-batch-size", type=int, default=4096)
+    parser.add_argument("--global-batch-size", type=int, default=3072)
     parser.add_argument("--global-seed", type=int, default=0)
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema") 
     parser.add_argument("--num-workers", type=int, default=32)
